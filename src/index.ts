f54c179244187c46052d2eab59757b502939abdf
MelFadanelli <97249091+MelFadanelli@users.noreply.github.com>
2022-03-29 00:51:18 +0000

    ...

--- src/index.ts
+++ src/index.ts
@@ -19,9 +19,11 @@ function buttonSubmitClick() {
         if (bPalindrome) {
             divResult.innerText = 'The word is palindrome!'
             //TODO: Implementa la asignación del atributo className con valor 'palindrome'
+            divResult.className = "palindrome";
         } else {
             //TODO: Asigna a la propiedad innerText de divResult el valor 'The word is not palindrome, sorry.'
             divResult.className = "notPalindrome";
+            divResult.innerText = 'The word is not palindrome, sorry.'
         }
     }
 }
